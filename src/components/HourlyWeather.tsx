f9af07cd9816f8980a54ad143439c9b92fbc7124
nauticuff <felipecarlos200x@gmail.com>
2023-05-30 11:09:29 -0700

    added key attribute

--- src/components/HourlyWeather.tsx
+++ src/components/HourlyWeather.tsx
@@ -15,11 +15,11 @@ const HourlyWeather: React.FC<HourlyWeatherProps> = ({ hourlyWeather }) => {
   return (
     <div className='w-100 h-48 bg-gray-500'>
         {hourlyWeather.map((row: any) => (
-            <div className='text-white'>
+            <div key={row.hour} className='text-white'>
                 <p>{row.hour}</p>
-                <p></p>
-                <p></p>
-                <p></p>
+                <p>{row.icon}</p>
+                <p>{row.precip}</p>
+                <p>{row.timestamp}</p>
             </div>
         ))}
     </div>
